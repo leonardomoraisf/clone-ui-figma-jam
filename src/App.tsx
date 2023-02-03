ca72b0228207f7c1b7a274a132a62b98601839e2
Leonardo Morais Franca <mfrancaleonardo@gmail.com>
2023-02-03 16:13:59 -0300

    circle and diamond

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,15 @@ import 'reactflow/dist/style.css';
 import { Square } from './components/nodes/Square';
 import { useCallback } from 'react';
 import DefaultEdge from './edges/DefaultEdge';
+import { Diamond } from './components/nodes/Diamond';
+import { Circle } from './components/nodes/Circle';
 
 // Nodes(coisas na tela), Edges(conexões)
 
 const NODE_TYPES = {
   square: Square,
+  diamond: Diamond,
+  circle: Circle,
 }
 
 const EDGE_TYPES = {
@@ -22,19 +26,8 @@ const INITIAL_NODES = [
     id: crypto.randomUUID(),
     type: 'square',
     position: {
-      x: 200,
-      y: 400,
-    },
-    data: {
-
-    },
-  },
-  {
-    id: crypto.randomUUID(),
-    type: 'square',
-    position: {
-      x: 1000,
-      y: 400,
+      x: 400,
+      y: 200,
     },
     data: {
 
@@ -60,6 +53,44 @@ function App() {
         type: 'square',
         position: {
           x: 750,
+          y: 350,
+        },
+        data: {
+    
+        },
+      },
+    ])
+
+  }
+
+  function addDiamondNode(){
+
+    setNodes(nodes => [
+      ...nodes,
+      {
+        id: crypto.randomUUID(),
+        type: 'diamond',
+        position: {
+          x: 1050,
+          y: 350,
+        },
+        data: {
+    
+        },
+      },
+    ])
+
+  }
+
+  function addCircleNode(){
+
+    setNodes(nodes => [
+      ...nodes,
+      {
+        id: crypto.randomUUID(),
+        type: 'circle',
+        position: {
+          x: 550,
           y: 350,
         },
         data: {
@@ -98,11 +129,21 @@ function App() {
 
       </ReactFlow>
 
-      <Toolbar.Root className='fixed bottom-20 left-1/2 -translate-x-1/2 bg-white rounded-2xl shadow-lg border border-zinc-300 px-8 h-20 w-96 overflow-hidden'>
+      <Toolbar.Root className='fixed bottom-20 left-1/2 -translate-x-1/2 bg-white rounded-2xl shadow-lg border border-zinc-300 px-8 h-20 w-120 overflow-hidden'>
 
         <Toolbar.Button 
           className="w-32 h-32 bg-violet-500 mt-6 rounded transition-transform hover:-translate-y-2" 
           onClick={addSquareNode}
+        />
+
+        <Toolbar.Button 
+          className="w-32 h-32 bg-violet-500 mt-10 ml-12 rounded transition-transform hover:-translate-y-2 rotate-45" 
+          onClick={addDiamondNode}
+        />
+
+        <Toolbar.Button 
+          className="w-32 h-32 bg-violet-500 mt-10 ml-12 rounded-full transition-transform hover:-translate-y-2" 
+          onClick={addCircleNode}
         />
 
       </Toolbar.Root>
